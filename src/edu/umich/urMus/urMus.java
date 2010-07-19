6ea9f4ab91385d1b1a9db6d3593b78533a5e1a83
Jong Wook Kim <jongwook@umich.edu>
2010-07-19 14:38:49 -0400

    initial commit

---
+++ src/edu/umich/urMus/urMus.java
@@ -0,0 +1,15 @@
+package edu.umich.urMus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class urMus extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
