aa04f848838984496f657ae69d026a71cfa3a79a
Jong Wook Kim <jongwook@umich.edu>
2010-07-19 15:46:22 -0400

    compiles with lua

--- jni/urMus.c
+++ jni/urMus.c
@@ -2,6 +2,18 @@
 #include <string.h>
 #include <jni.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus *
+
+#include "lua/lua.h"
+#include "lua/lualib.h"
+#include "lua/lauxlib.h"
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
  * file located at:
